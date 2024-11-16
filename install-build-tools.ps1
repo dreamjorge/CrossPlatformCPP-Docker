e551f47f34be16dff99b4fab539c113da76dbd0c
dreamjorge <47013262+dreamjorge@users.noreply.github.com>
2024-11-16 16:02:20 -0600

    Actualizar install-build-tools.ps1

--- install-build-tools.ps1
+++ install-build-tools.ps1
@@ -1,5 +1,3 @@
-# install-build-tools.ps1
-
 # Create TEMP directory for downloads; use -Force to avoid errors if it exists
 New-Item -ItemType Directory -Path C:\TEMP -Force
 
@@ -8,7 +6,7 @@ Invoke-WebRequest -Uri $env:CHANNEL_URL -OutFile C:\TEMP\VisualStudio.chman
 Invoke-WebRequest -Uri $env:VS_BUILD_TOOLS_URL -OutFile C:\TEMP\vs_buildtools.exe
 
 # Install Visual Studio Build Tools with the C++ workload
-C:\TEMP\vs_buildtools.exe --quiet --wait --norestart --nocache `
+& C:\TEMP\vs_buildtools.exe --quiet --wait --norestart --nocache `
     --channelUri C:\TEMP\VisualStudio.chman `
     --installChannelUri C:\TEMP\VisualStudio.chman `
     --add Microsoft.VisualStudio.Workload.VCTools `
@@ -24,10 +22,5 @@ Expand-Archive -Path C:\TEMP\cmake.zip -DestinationPath "C:\Program Files" -Forc
 # Add CMake to PATH
 $env:PATH = "C:\Program Files\cmake-${env:CMAKE_VERSION}-windows-x86_64\bin;" + $env:PATH
 
-# Clean up TEMP directory and CMake ZIP installer
+# Clean up TEMP directory
 Remove-Item -Recurse -Force C:\TEMP
-
-# Verify that VsDevCmd.bat exists at the expected path
-if (-Not (Test-Path "C:\BuildTools\Common7\Tools\VsDevCmd.bat")) {
-    Write-Error "VsDevCmd.bat not found at C:\BuildTools\Common7\Tools\VsDevCmd.bat"
-}
