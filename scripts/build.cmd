3b7acfb07fe5281f0559fc0dbbe3ef8781fcc565
dreamjorge <47013262+dreamjorge@users.noreply.github.com>
2024-11-17 13:59:10 -0600

    Actualizar build.cmd

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -9,6 +9,17 @@ IF NOT "%CONFIG%"=="" (
 )
 
 echo BUILD_TYPE is %BUILD_TYPE%
+echo Current Directory: %CD%
+echo Listing files in current directory:
+dir
+
+echo Verifying CMakeLists.txt presence:
+if exist "CMakeLists.txt" (
+    echo CMakeLists.txt found.
+) else (
+    echo ERROR: CMakeLists.txt not found!
+    exit /b 1
+)
 
 CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" && ^
 cmake -S . -B build -G "Visual Studio 17 2022" -A x64 && ^
