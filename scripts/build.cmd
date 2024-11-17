872de3d986da243df80ffbbe4016dcc517a52cd8
Jorge Ugalde <jorge-kun@live.com>
2024-11-17 09:59:33 -0600

    refactor: simplify Docker workflow and enhance build script configuration handling

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,7 +1,13 @@
 @echo off
-SET BUILD_TYPE=%1
-IF "%BUILD_TYPE%"=="" SET BUILD_TYPE=Release
 
-CALL C:\BuildTools\Common7\Tools\VsDevCmd.bat &&
+IF NOT "%1"=="" (
+    SET BUILD_TYPE=%1
+) ELSE IF NOT "%CONFIG%"=="" (
+    SET BUILD_TYPE=%CONFIG%
+) ELSE (
+    SET BUILD_TYPE=Release
+)
+
+CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" &&
 cmake -S . -B build -G "Visual Studio 16 2019" -A x64 &&
 cmake --build build --config %BUILD_TYPE%
