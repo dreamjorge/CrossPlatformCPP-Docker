9dc46dc5f98aef815b4cf30bdf55f211da2f9686
dreamjorge <47013262+dreamjorge@users.noreply.github.com>
2024-11-17 11:45:07 -0600

    Actualizar build.cmd

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -10,6 +10,4 @@ IF NOT "%CONFIG%"=="" (
 
 echo BUILD_TYPE is %BUILD_TYPE%
 
-CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" && ^
-    cmake -S . -B build -G "Visual Studio 17 2022" -A x64 && ^
-    cmake --build build --config %BUILD_TYPE%
+CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" && cmake -S . -B build -G "Visual Studio 17 2022" -A x64 && cmake --build build --config %BUILD_TYPE%
