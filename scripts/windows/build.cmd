6b5a06bd6ffe63ded1892f3ab9786ba2a2409895
dreamjorge <47013262+dreamjorge@users.noreply.github.com>
2024-11-17 20:56:17 -0600

    Update windows

---
+++ scripts/windows/build.cmd
@@ -0,0 +1,31 @@
+@echo off
+
+echo CONFIG is %CONFIG%
+
+IF NOT "%CONFIG%"=="" (
+    SET BUILD_TYPE=%CONFIG%
+) ELSE (
+    SET BUILD_TYPE=Release
+)
+
+echo BUILD_TYPE is %BUILD_TYPE%
+echo BUILD_DIR is %BUILD_DIR%
+echo Current Directory: %CD%
+echo Listing files in current directory:
+dir
+
+echo Verifying CMakeLists.txt presence:
+if exist "CMakeLists.txt" (
+    echo CMakeLists.txt found.
+) else (
+    echo ERROR: CMakeLists.txt not found!
+    exit /b 1
+)
+
+echo Starting build process...
+echo Before VsDevCmd.bat: %CD%
+CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat"
+echo After VsDevCmd.bat: %CD%
+
+cmake -S . -B build -G "Visual Studio 17 2022" -A x64
+cmake --build build --config %BUILD_TYPE%
