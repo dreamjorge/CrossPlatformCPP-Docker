a917ed04dc913e8196110124bf7ecd00a5409b03
dreamjorge <47013262+dreamjorge@users.noreply.github.com>
2024-11-17 18:27:22 -0600

    Update build.cmd

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -23,6 +23,9 @@ if exist "CMakeLists.txt" (
 )
 
 echo Starting build process...
-CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" && ^
-cmake -S . -B build -G "Visual Studio 17 2022" -A x64 && ^
+echo Before VsDevCmd.bat: %CD%
+CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat"
+echo After VsDevCmd.bat: %CD%
+
+cmake -S . -B build -G "Visual Studio 17 2022" -A x64
 cmake --build build --config %BUILD_TYPE%
