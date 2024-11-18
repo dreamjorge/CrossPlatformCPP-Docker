00bf5a617354600d8c599c240d66224e21bd9fab
dreamjorge <47013262+dreamjorge@users.noreply.github.com>
2024-11-18 00:08:33 -0600

    Update build.cmd

--- scripts/windows/build.cmd
+++ scripts/windows/build.cmd
@@ -22,10 +22,13 @@ if exist "CMakeLists.txt" (
     exit /b 1
 )
 
-echo Starting build process...
-echo Before VsDevCmd.bat: %CD%
-CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat"
-echo After VsDevCmd.bat: %CD%
+echo Verifying CMake installation:
+cmake --version || (
+    echo ERROR: CMake is not installed or not in PATH!
+    exit /b 1
+)
 
-cmake -S . -B build -G "Visual Studio 17 2022" -A x64
-cmake --build build --config %BUILD_TYPE%
+echo Starting build process...
+CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" && `
+    cmake -S . -B build -G "Visual Studio 17 2022" -A x64 && `
+    cmake --build build --config %BUILD_TYPE%
