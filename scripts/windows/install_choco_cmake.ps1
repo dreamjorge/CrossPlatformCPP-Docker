89961d91b89a1d38b15fa96b097f456a396b5832
Jorge Ugalde <jorge-kun@live.com>
2024-11-19 09:29:34 -0600

    Enhance Docker workflow for Windows: rename job, improve build steps, and set default CMAKE_VERSION in installation script

--- scripts/windows/install_choco_cmake.ps1
+++ scripts/windows/install_choco_cmake.ps1
@@ -1,4 +1,7 @@
-# install_choco_cmake.ps1
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+if (-not $env:CMAKE_VERSION) {
+    Write-Host "INFO: CMAKE_VERSION not set. Defaulting to 3.21.3."
+    $env:CMAKE_VERSION = "3.21.3"
+}
 choco install cmake --version=$env:CMAKE_VERSION --installargs 'ADD_CMAKE_TO_PATH=System' -y
