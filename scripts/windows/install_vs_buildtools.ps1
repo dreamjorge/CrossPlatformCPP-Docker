1c28803317ca86604264a4d2a909180927acfd7b
dreamjorge <47013262+dreamjorge@users.noreply.github.com>
2024-11-23 21:00:23 -0600

    Update install_vs_buildtools.ps1

--- scripts/windows/install_vs_buildtools.ps1
+++ scripts/windows/install_vs_buildtools.ps1
@@ -38,8 +38,8 @@ Function Install-BuildTools {
     $installLogPath = "C:\temp\vs_buildtools_install.log"
     & $InstallerPath $InstallArgs > $installLogPath 2>&1
     if ($LASTEXITCODE -ne 0) {
-        Write-Host "Installation Log:"
-        Get-Content $installLogPath
+        Log-Error "Build Tools installation failed. Installation Log Content:"
+        Get-Content $installLogPath -Tail 100
         throw "Failed to install Build Tools. Check the log: $installLogPath"
     }
     Log-Info "Build Tools installed successfully."
@@ -60,16 +60,4 @@ Function Get-VsInstallerUrl {
 Log-Info "VsYear is $VsYear"
 Log-Info "VsVersion is $VsVersion"
 
-$vsBuildToolsUrl = Get-VsInstallerUrl -VsVersion $VsVersion
-$buildToolsPath = "C:\temp\vs_buildtools_$VsVersion.exe"
-
-# Download and install Visual Studio Build Tools
-Download-File -Url $vsBuildToolsUrl -Destination $buildToolsPath
-$installArgs = "--quiet --wait --norestart `
-    --add Microsoft.VisualStudio.Workload.VCTools `
-    --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 `
-    --add Microsoft.VisualStudio.Component.Windows10SDK.19041 `
-    --add Microsoft.VisualStudio.Component.NuGet.BuildTools"
-Install-BuildTools -InstallerPath $buildToolsPath -InstallArgs $installArgs
-
-Log-Info "Visual Studio Build Tools setup completed successfully."
+$vsBuildToolsUrl = Get-VsInstallerUrl -
