165cb9678e50e900825781605c9985574b473ea7
dreamjorge <47013262+dreamjorge@users.noreply.github.com>
2024-11-29 07:59:01 -0600

    Update install_vs_buildtools.ps1

--- scripts/windows/install_vs_buildtools.ps1
+++ scripts/windows/install_vs_buildtools.ps1
@@ -51,9 +51,11 @@ $installerArguments = @(
     "--wait",
     "--norestart",
     "--nocache",
-    "--add", "Microsoft.VisualStudio.Workload.AzureBuildTools",
+    "--add", "Microsoft.VisualStudio.Workload.VCTools",
     "--add", "Microsoft.VisualStudio.Component.VC.Tools.x86.x64",
     "--add", "Microsoft.VisualStudio.Component.Windows10SDK.19041",
+    "--add", "Microsoft.VisualStudio.Workload.MSBuildTools",
+    "--add", "Microsoft.VisualStudio.Component.MSBuild",
     "--installPath", "C:\Program Files (x86)\Microsoft Visual Studio\$VS_YEAR\BuildTools",
     "--log", $logPath
 )
