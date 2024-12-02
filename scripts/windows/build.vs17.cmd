7676f887db0aeb6a9e178d6082d36224e9558a1c
Jorge Ugalde <jorge-kun@live.com>
2024-12-02 10:30:16 -0600

    feat: update build scripts for Visual Studio versions and remove obsolete script

--- scripts/windows/build.vs17.cmd
+++ scripts/windows/build.vs17.cmd
@@ -1,23 +1,19 @@
 @echo off
 
-:: Print the configuration
 echo CONFIG is %CONFIG%
 
-:: Set the build type based on CONFIG or default to Release
 IF NOT "%CONFIG%"=="" (
     SET BUILD_TYPE=%CONFIG%
 ) ELSE (
     SET BUILD_TYPE=Release
 )
 
-:: Display the selected build type and directory details
 echo BUILD_TYPE is %BUILD_TYPE%
 echo BUILD_DIR is %BUILD_DIR%
 echo Current Directory: %CD%
 echo Listing files in current directory:
 dir
 
-:: Verify the presence of CMakeLists.txt
 echo Verifying CMakeLists.txt presence:
 if exist "CMakeLists.txt" (
     echo CMakeLists.txt found.
@@ -26,24 +22,13 @@ if exist "CMakeLists.txt" (
     exit /b 1
 )
 
-:: Verify CMake installation
 echo Verifying CMake installation:
-cmake --version >nul 2>&1
-if errorlevel 1 (
+cmake --version || (
     echo ERROR: CMake is not installed or not in PATH!
     exit /b 1
 )
 
-:: Start the Visual Studio environment and build process
 echo Starting build process...
-CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" ^
-    && cmake -S . -B build -G "Visual Studio 15 2017" -A x64 ^
-    && cmake --build build --config %BUILD_TYPE%
-
-:: Check if the build was successful
-if errorlevel 1 (
-    echo ERROR: Build failed!
-    exit /b 1
-) else (
-    echo Build completed successfully.
-)
+CALL "C:\BuildTools\Common7\Tools\VsDevCmd.bat" && `
+    cmake -S . -B build -G "Visual Studio 17 2022" -A x64 && `
+    cmake --build build --config %BUILD_TYPE%
